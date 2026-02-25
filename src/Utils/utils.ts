389190b78bdc5d377f1e7409f588d0a27ea365d5
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-02-25 10:44:26 +0100

    refactor: updateAvailable function

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,9 +1,7 @@
 import semver from 'semver';
 
 export function updateAvailable(current: string, latest: string): 'up-to-date' | 'update-available' {
-    if (semver.gt(latest, current)) {
-        return 'up-to-date';
-    } else {
-        return 'update-available';
-    }
+    if (semver.eq(current, latest)) return 'up-to-date';
+
+    return semver.gt(current, latest)? 'up-to-date' : 'update-available';
 }
