0b9bc07443282ba9bfcfb81ae2c5ca67d2c45cd9
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-02-26 18:01:59 +0100

    modify: test method & allows repeated commands using 'same'

--- src/Utils/commander.ts
+++ src/Utils/commander.ts
@@ -4,11 +4,21 @@ class Commander {
     static OS: NodeJS.Platform = process.platform;
     static isWindows: boolean = Commander.OS === 'win32';
 
-    static run (command: Record<'windows' | 'linux', string>) {
+    static run (command: Record<'windows' | 'linux', string | 'same'>, params?: child_process.ExecSyncOptionsWithBufferEncoding) {
         if (this.isWindows) {
-            child_process.execSync(command.windows);
+            if (command.windows === 'same') throw new Error('La propiedad "windows" no puede ser "same" si el sistema operativo es Windows.');
+            child_process.execSync(command.windows, params);
         } else {
-            child_process.execSync(command.linux);
+            child_process.execSync(command.linux === 'same' ? command.windows : command.linux, params);
+        }
+    }
+
+    static test(command: Record<'windows' | 'linux', string>): boolean {
+        try {
+            this.run(command, { stdio: 'ignore' });
+            return true;
+        } catch (e) {
+            return false;
         }
     }
 }
