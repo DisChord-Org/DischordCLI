2ff74474da83628c27c6a64b7c00710d5c6a4532
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-02-26 13:59:07 +0100

    refactor: homedir

--- src/Utils/homedir.ts
+++ src/Utils/homedir.ts
@@ -5,35 +5,50 @@ import fs from 'fs';
 import { gray, red, yellow } from './drawer';
 
 class homedir {
+    private isWin: boolean = Commander.isWindows;
+
     public _homedir: string = os.homedir();
-    public _DisChordCompilerFolder: string = this._homedir;
-    public _DisChordCompilerPath: string = this._homedir;
-    public _DisChordCLIFolder: string = this._homedir;
-    public _DisChordCLIPath: string = this._homedir;
+    public _DisChordRoot: string = path.join(this._homedir, '.dischord');
+    public _BinFolder: string = this._homedir;
 
     constructor () {
-        if (Commander.isWindows) {
-            this._DisChordCompilerFolder = path.join(this._homedir, '.dischord', 'bin', 'DisChordCompiler');
-            this._DisChordCLIFolder = path.join(this._homedir, '.dischord', 'bin', 'DisChordCLI');
-        } else {
-            this._DisChordCompilerFolder = path.join(this._homedir, '.local', 'bin', 'DisChordCompiler');
-            this._DisChordCLIFolder = path.join(this._homedir, '.local', 'bin', 'DisChordCLI');
-        }
+        // bins on:
+        // w: ~/.dischord/bin
+        // l: ~/.local/bin
+        this._BinFolder = this.isWin
+            ? path.join(this._DisChordRoot, 'bin')
+            : path.join(this._homedir, '.local', 'bin');
 
-        if (!fs.existsSync(this._DisChordCompilerFolder)) {
-            console.log(yellow('ADVERTENCIA: ') + red('No se encontró la carpeta de DisChord Compiler en tu directorio de usuario. Creando carpeta...'));
-            fs.mkdirSync(this._DisChordCompilerFolder, { recursive: true });
-            console.log(yellow('SOLUCIONADO: ') + red('Carpeta de DisChord Compiler creada en: ') + gray(this._DisChordCompilerFolder));
-        }
+        this.checkPath(this._DisChordRoot, 'folder');
+        this.checkPath(this._BinFolder, 'folder');
+    }
 
-        if (!fs.existsSync(this._DisChordCLIFolder)) {
-            console.log(yellow('ADVERTENCIA: ') + red('No se encontró la carpeta de DisChord CLI en tu directorio de usuario. Creando carpeta...'));
-            fs.mkdirSync(this._DisChordCLIFolder, { recursive: true });
-            console.log(yellow('SOLUCIONADO: ') + red('Carpeta de DisChord CLI creada en: ') + gray(this._DisChordCLIFolder));
+    private checkPath (_path: string, type: 'folder' | 'file') {
+        if (!fs.existsSync(_path)) {
+            console.log(yellow('ADVERTENCIA: ') + red(`No se encontró ${type == 'file'? 'el fichero' : 'la carpeta'} en tu sistema. Creando...`));
+            if (type === 'file') {
+                fs.writeFileSync(_path, '', 'utf-8');
+            } else fs.mkdirSync(_path, { recursive: true });
+            console.log(yellow('SOLUCIONADO: ') + red('Archivo creado en: ') + gray(_path));
         }
+    }
 
-        this._DisChordCompilerPath = path.join(this._DisChordCompilerFolder, 'dischord_compiler');
-        this._DisChordCLIPath = path.join(this._DisChordCLIFolder, 'chord');
+    public existsVersion (component: 'cli' | 'compiler' | 'ide'): boolean {
+        return fs.existsSync(path.join(this._DisChordRoot, `${component}-version`));
+    }
+
+    public getVersionPath (component: 'cli' | 'compiler' | 'ide'): string {
+        return path.join(this._DisChordRoot, `${component}-version`);
+    }
+
+    public existsBinary (component: 'chord' | 'dischord-compiler'): boolean {
+        const ext = Commander.isWindows ? '.exe' : '';
+        return fs.existsSync(path.join(this._BinFolder, `${component}${ext}`));
+    }
+
+    public getBinaryPath(component: 'chord' | 'dischord-compiler'): string {
+        const ext = Commander.isWindows ? '.exe' : '';
+        return path.join(this._BinFolder, `${component}${ext}`);
     }
 }
 
